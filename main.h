72ea5bfa5d848418a2d29c6813d4db8af0c8b1dc
under-ground-dog <davemotari22@gmail.com>
2022-04-17 20:55:13 -0700

    Weeh

--- main.h
+++ main.h
@@ -1,5 +1,34 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+#include <stdarg.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <unistd.h>
+
+
+
+/**
+* struct op - Struct op
+*
+*
+* @op: The operator
+*
+* @f: The function associated
+*
+*/
+
+typedef struct op
+
+{
+char *op;
+int (*f)(va_list);
+} op_t;
 
 int _printf(const char *format, ...);
+
 #endif
