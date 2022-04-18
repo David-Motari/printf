febc7aaf903b68f681886605272154c1a9972857
17Keith <kayomega7@gmail.com>
2022-04-18 11:36:53 -0700

    weeh

--- _printf.c
+++ _printf.c
@@ -1,48 +1,134 @@
 #include "main.h"
-#include "Main.h"
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+* pt_chr - print a character
+*@va: character
+*
+* Return: no return
+*/
+int pt_chr(va_list va)
+{
+int c;
+
+c = va_arg(va, int);
+_putchar(c);
+return (1);
+}
+/**
+* pt_string - print a string
+*@va: pointer to string
+*
+* Return: no return
+*/
+int pt_string(va_list va)
+{
+int i, j;
+char n[] = "(null)";
+char *s = va_arg(va, char *);
+
+if (s == NULL)
+{
+for (i = 0; n[i] != '\0'; i++)
+_putchar(n[i]);
+return (6);
+}
+for (j = 0; s[j] != '\0'; j++)
+_putchar(s[j]);
+return (j);
+}
+/**
+* print_number - Entry point
+*@va: the integer to print
+* Return: no return
+*/
+int print_number(va_list va)
+{
+int i, len, r, l;
+unsigned int abs, num, numt;
+int n = va_arg(va, int);
+
+len = 0;
+i = 0;
+r = 1;
+l = 1;
+if (n < 0)
+{
+_putchar('-');
+len++;
+abs = -n;
+} else
+{
+abs = n;
+}
+
+num = abs;
+while (num > 0)
+{
+num /= 10;
+i++;
+}
+
+while (r < i)
+{
+l *= 10;
+r++;
+}
+while (l >= 1)
+{
+numt = (abs / l) % 10;
+_putchar(numt + '0');
+len++;
+l /= 10;
+}
+return (len);
+}
+
+/**
+ * _printf - print output according to a format
+ *@format: first argument
+ *
+ * Return: the number of characters printed(excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
-
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+int i = 0, j, len = 0, count;
+va_list valist;
+types difftypes[] = {{'c', pt_chr}, {'s', pt_string}, {'d', print_number},
+{'i', print_number}}; 
+/*
+* {'b', binary}, {'u', print_unsigned},
+*{'x', hexa}, {'X', hexa_upper}, {'o', octal}, {'R', print_rot},
+*{'r', print_rev}, {'S', stringhexa}, {'p', pointer}};
+*/
+if (format == NULL || (format[0] == '%' && format[1] == 0))
+return (-1);
+va_start(valist, format);
+while (format != NULL && format[i])
+{
+if (format[i] != '%')
+len += _putchar(format[i]);
+else
+{
+i++;
+if (format[i] == '%')
+len += _putchar('%');
+j = 0;
+count = 0;
+while (j < 13)
+{
+if (format[i] == difftypes[j].t)
+{
+len += difftypes[j].f(valist);
+count = 1;
+break; }
+j++; }
+if (!count && format[i] != '%')
+{
+len++;
+len++;
+_putchar('%');
+_putchar(format[i]); }}
+i++; }
+va_end(valist);
+return (len);
 }
