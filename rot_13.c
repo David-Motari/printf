e8a5099547187fc0d00c3e33b2e8117ea8784744
17Keith <kayomega7@gmail.com>
2022-04-18 07:15:11 -0700

    Trail for Rot_13

---
+++ rot_13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @p: input string
+ * Return: encoded string
+ */
+char *rot13(char *p)
+{
+	int i;
+
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = p;
+
+	while (*p)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*p == rot13[i])
+			{
+				*p = ROT13[i];
+				break;
+			}
+		}
+		p++;
+	}
+	return (ptr);
+}
