2b197134826d4d63b6a0f08e794300030932fbe8
17Keith <kayomega7@gmail.com>
2022-04-19 01:43:25 -0700

    Trail for task 7

--- print_S.c
+++ print_S.c
@@ -7,29 +7,13 @@
  *
  */
 
-int print_bigS(va_list l, flags_t *f)
+void _puts(char *str)
 {
-	int i, count = 0;
-	char *res;
-	char *s = va_arg(l, char *);
+	int i;
 
-	(void)f;
-	if (!s)
-		return (_puts("(null)"));
-
-	for (i = 0; s[i]; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (s[i] > 0 && (s[i] < 32 || s[i] >= 127))
-		{
-			_puts("\\x");
-			count += 2;
-			res = convert(s[i], 16, 0);
-			if (!res[1])
-				count += _putchar('0');
-			count += _puts(res);
-		}
-		else
-			count += _putchar(s[i]);
+		_putchar (str[i]);
 	}
-	return (count);
+	_putchar ('\n');
 }
